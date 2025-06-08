5580eb69f6e889cea5f1f4338bbc710928a53192
muhammadsaad <mu7ammadsa3dd@gmail.com>
2025-06-08 18:50:00 +0300

    add test case comment

--- GroceryStore.Api/Features/GetAvailableSlots/Endpoint.cs
+++ GroceryStore.Api/Features/GetAvailableSlots/Endpoint.cs
@@ -5,6 +5,17 @@ using GroceryStore.Application.Features.GetAvailableSlots;
 
 namespace GroceryStore.Api.Features.GetAvailableSlots;
 
+/* test case
+{
+ "orderDate": "2025-06-09T15:35:43.328Z",
+ "ids": [
+   "617d666f-bef4-4c33-bf60-dc8e6d4a321c",
+   "b99798cb-c13b-4db4-ad99-dfc9b7f849d7",
+   "d28c8f16-84a0-4247-8fd8-0b39a3d2fdbe",
+   "f06d0e2b-4f00-412d-9467-c00189f8df59"
+ ]
+}
+*/
 public class Endpoint(ISender sender) : ControllerBase
 {
     private CancellationToken CancellationToken => HttpContext.RequestAborted;
