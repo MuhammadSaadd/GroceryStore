88b9e97638fbd63c6d24c9e04cff00919dc12dc4
muhammadsaad <mu7ammadsa3dd@gmail.com>
2025-06-17 15:03:03 +0300

    update the test case

--- GroceryStore.Domain/Entities/FreshFoodProduct.cs
+++ GroceryStore.Domain/Entities/FreshFoodProduct.cs
@@ -9,7 +9,7 @@ public class FreshFoodProduct(
 {
     public override bool CanBeDeliveredOn(Slot slot, DateOnly orderDate, TimeOnly orderTime)
     {
-        if (orderTime >= new TimeOnly(Constants.FreshFoodSameDayDeliveryCutoff, 0))
+        if (orderTime >= new TimeOnly(Constants.FreshFoodSameDayDeliveryCutoff, 0)) // 1
             return slot.Date > orderDate;
         return true;
     }
